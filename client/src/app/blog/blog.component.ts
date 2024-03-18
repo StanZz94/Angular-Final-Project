44d1a4ed61ede015af1dbab87454c4faa82fa979
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-03-18 18:10:58 +0200

    add blog

--- client/src/app/blog/blog.component.ts
+++ client/src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Post } from '../types/post';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-blog',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
+  latestPosts: Post[] = [];
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllPosts();
+  }
+
+  getAllPosts(): void {
+    this.api.getPosts()
+      .subscribe(posts => {
+        this.latestPosts = posts;
+      });
+  }
 }
