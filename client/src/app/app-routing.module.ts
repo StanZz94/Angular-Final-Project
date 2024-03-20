c217abcea7ef1a8fdf666c3f44ee789e56f22a0a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-03-20 11:12:15 +0200

    styling 404 page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { BlogComponent } from './blog/blog.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   {path: '', pathMatch: "full", redirectTo: '/home'},
   {path: 'home', component: MainComponent},
   {path: 'blog', component: BlogComponent},
+  {path: 'about', component: AboutUsComponent},
   {path: '**', redirectTo: '/404'},
   {path: '404', component: ErrorPageComponent},
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 ;
-import { ErrorPageComponent } from './error-page/error-page.component'import { NgModule } from '@angular/core';
+import { ErrorPageComponent } from './error-page/error-page.component'
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +14,6 @@ import { LatestBlogComponent } from './main/latest-blog/latest-blog.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { UserModule } from './user/user.module';
 import { BlogComponent } from './blog/blog.component';
-import { 404Component } from './404/404.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { 404Component } from './404/404.component';
     LatestBlogComponent,
     AboutUsComponent,
     BlogComponent,
-    404Component, ErrorPageComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
