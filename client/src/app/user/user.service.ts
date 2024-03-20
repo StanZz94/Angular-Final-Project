8d832678d33634ccc78bbf88b521e2d542e61238
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-03-20 19:59:49 +0200

    mocked login

--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { UserAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  user: UserAuth | undefined;
+  USER_KEY = '[user]'
+
+  get isLogged():boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+
+    try{
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (err) {
+      this.user = undefined;
+    }
+   }
+
+   login() {
+
+    this.user = {
+      name: "Stanko",
+      lastName: "Traikov",
+      email: "stanko@abv.bg",
+      password: "1234",
+    }
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+   }
+
+   logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+   }
 }
